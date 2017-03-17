9ccea33420fd3400406c02fa5fb494941417a13f
Shorouk Elsayed <shoroukkhalifa@gmail.com>
2017-03-17 22:22:18 +0200

    plot3.r

---
+++ plot3.r
@@ -0,0 +1,7 @@
+plot(x=house_power$timestamp, y=house_power$Sub_metering_1, type="n", main="Energy Sub Metering x Day", ylab="Energy Sub Metering", xlab="")
+lines(x=house_power$timestamp, y=house_power$Sub_metering_1, col="green")
+lines(x=house_power$timestamp, y=house_power$Sub_metering_2, col="red")
+lines(x=house_power$timestamp, y=house_power$Sub_metering_3, col="blue")
+legend(lwd=0.7, "topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("green", "red", "blue"), cex=0.7)
+dev.copy(device = png, filename="plot3.png", width=480, height=480)
+dev.off()
