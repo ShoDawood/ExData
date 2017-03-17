4723f54a0f77d12e8e070584a24b412cd3d2cbef
Shorouk Elsayed <shoroukkhalifa@gmail.com>
2017-03-17 22:46:23 +0200

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -1,12 +1,29 @@
-par(mfrow=c(2,2), mar=c(5,4,4,2), cex=0.7)
+  rm(list=ls())
 
-plot(x=house_power$timestamp, y=house_power$Global_active_power, type="l", main=NULL, ylab="Global Active Power", xlab="")
-plot(x=house_power$timestamp, y=house_power$Voltage, type="l", main=NULL, ylab="Voltage", xlab="datetime")
-plot(x=house_power$timestamp, y=house_power$Sub_metering_1, type="n", main=NULL, ylab="Energy Sub Metering", xlab="")
-lines(x=house_power$timestamp, y=house_power$Sub_metering_1, col="green")
-lines(x=house_power$timestamp, y=house_power$Sub_metering_2, col="red")
-lines(x=house_power$timestamp, y=house_power$Sub_metering_3, col="blue")
-legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("green", "red", "blue"), cex=0.4)
-plot(x=house_power$timestamp, y=house_power$Global_reactive_power, type="l", main=NULL, ylab="Global Reactive Power", xlab="datetime")
-dev.copy(device = png, filename="plot4.png", width=480, height=480)
-dev.off()
+  tbl = read.table(file = "household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+  tbl$Time = strptime(paste(tbl$Date,tbl$Time,sep=" "),format = "%d/%m/%Y %H:%M:%S")
+  tbl$Date = as.Date(tbl$Date,format = "%d/%m/%Y")
+
+  tbl = rbind(subset(tbl,Date==as.Date("2007-02-01")),subset(tbl,Date==as.Date("2007-02-02")))
+
+
+  png(filename = "ExData_Plotting1/plot4.png", width= 480, height = 480, units = "px")
+  par(mfrow = c(2,2))
+  with(tbl, {
+    plot(Time,Global_active_power,type="n",ylab="Global Active Power",xlab="")
+    lines(Time,Global_active_power)
+    
+    plot(Time,Voltage,xlab="datetime",type="n",ylab="Voltage")
+    lines(Time,Voltage)
+    
+    plot(Time,Sub_metering_1,type="n",ylab="Energy sub metering",xlab="")
+    lines(Time,Sub_metering_1,col="black")
+    lines(Time,Sub_metering_2,col="red")
+    lines(Time,Sub_metering_3,col="blue")
+    legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty = "n",
+           lty=1,col=c("black","red","blue"))
+    
+    plot(Time,Global_reactive_power,type="n",xlab="datetime")
+    lines(Time,Global_reactive_power)
+  })
+  dev.off()
