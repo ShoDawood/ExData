f98833ba4c54b77feecc26b7a504729651a2aa72
Shorouk Elsayed <shoroukkhalifa@gmail.com>
2017-03-17 22:24:21 +0200

    plot4.r

---
+++ plot4.r
@@ -0,0 +1,12 @@
+par(mfrow=c(2,2), mar=c(5,4,4,2), cex=0.7)
+
+plot(x=house_power$timestamp, y=house_power$Global_active_power, type="l", main=NULL, ylab="Global Active Power", xlab="")
+plot(x=house_power$timestamp, y=house_power$Voltage, type="l", main=NULL, ylab="Voltage", xlab="datetime")
+plot(x=house_power$timestamp, y=house_power$Sub_metering_1, type="n", main=NULL, ylab="Energy Sub Metering", xlab="")
+lines(x=house_power$timestamp, y=house_power$Sub_metering_1, col="green")
+lines(x=house_power$timestamp, y=house_power$Sub_metering_2, col="red")
+lines(x=house_power$timestamp, y=house_power$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("green", "red", "blue"), cex=0.4)
+plot(x=house_power$timestamp, y=house_power$Global_reactive_power, type="l", main=NULL, ylab="Global Reactive Power", xlab="datetime")
+dev.copy(device = png, filename="plot4.png", width=480, height=480)
+dev.off()
