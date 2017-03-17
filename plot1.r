cff4143e71695e026817674f9f4895651baf0867
Shorouk Elsayed <shoroukkhalifa@gmail.com>
2017-03-17 22:44:00 +0200

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,5 +1,13 @@
-source("loadData.r")
-
-hist(house_power$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
-dev.copy(device = png, filename="plot1.png", width=480, height=480)
-dev.off()
+  rm(list=ls())
+# Load the Dataset
+  tbl = read.table(file = "household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+# Convert the Time Field to Time format and the Date Field to Date format
+  tbl$Time = strptime(paste(tbl$Date,tbl$Time,sep=" "),format = "%d/%m/%Y %H:%M:%S")
+  tbl$Date = as.Date(tbl$Date,format = "%d/%m/%Y")
+# Subset the Data
+  tbl = rbind(subset(tbl,Date==as.Date("2007-02-01")),subset(tbl,Date==as.Date("2007-02-02")))
+# Create a Graphics Device and Plot the Data
+  png(filename = "ExData_Plotting1/plot1.png", width= 480, height = 480, units = "px")
+  hist(tbl$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)",
+       col = "red")
+  dev.off()
