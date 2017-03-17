3493d700aa764150845929b5ad59014d23366107
Shorouk Elsayed <shoroukkhalifa@gmail.com>
2017-03-17 22:45:29 +0200

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -1,7 +1,16 @@
-plot(x=house_power$timestamp, y=house_power$Sub_metering_1, type="n", main="Energy Sub Metering x Day", ylab="Energy Sub Metering", xlab="")
-lines(x=house_power$timestamp, y=house_power$Sub_metering_1, col="green")
-lines(x=house_power$timestamp, y=house_power$Sub_metering_2, col="red")
-lines(x=house_power$timestamp, y=house_power$Sub_metering_3, col="blue")
-legend(lwd=0.7, "topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("green", "red", "blue"), cex=0.7)
-dev.copy(device = png, filename="plot3.png", width=480, height=480)
-dev.off()
+ rm(list=ls())
+  tbl = read.table(file = "household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+
+  tbl$Time = strptime(paste(tbl$Date,tbl$Time,sep=" "),format = "%d/%m/%Y %H:%M:%S")
+  tbl$Date = as.Date(tbl$Date,format = "%d/%m/%Y")
+
+  tbl = rbind(subset(tbl,Date==as.Date("2007-02-01")),subset(tbl,Date==as.Date("2007-02-02")))
+
+  png(filename = "ExData_Plotting1/plot3.png", width= 480, height = 480, units = "px")
+  with(tbl,plot(Time,Sub_metering_1,type="n",ylab="Energy sub metering",xlab=""))
+  with(tbl,lines(Time,Sub_metering_1,col="black"))
+  with(tbl,lines(Time,Sub_metering_2,col="red"))
+  with(tbl,lines(Time,Sub_metering_3,col="blue"))
+  legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+         lty=1,col=c("black","red","blue"))
+  dev.off()
